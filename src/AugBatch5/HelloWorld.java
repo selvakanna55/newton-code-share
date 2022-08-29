b4edb49a5c5792ff5681e151a5c4c6e660413266
selvakanna55 <selvakanna2409@gmail.com>
2022-08-29 23:25:58 +0530

    add simple output and datatype

---
+++ src/AugBatch5/HelloWorld.java
@@ -0,0 +1,7 @@
+package AugBatch5;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/AugBatch5/Main.java
@@ -0,0 +1,23 @@
+package AugBatch5;
+
+
+class Main {
+    public static void main(String args[]) {
+        String name = "Newton";
+        //  datatype variable name = value
+        char gender = 'M';
+        int rollNo = 15;
+        double average = 99.99;
+        int age = 18;
+        String college = "ABCD College";
+
+        System.out.println(name);
+        System.out.println(gender);
+        System.out.println(rollNo);
+        System.out.println(average);
+        System.out.println(age);
+        System.out.println(college);
+
+
+    }
+}
