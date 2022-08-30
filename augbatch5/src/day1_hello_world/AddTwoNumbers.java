a58e132afbfb0858a02ab02beca0e4ea7fad7f67
selvakanna55 <selvakanna2409@gmail.com>
2022-08-30 23:41:01 +0530

    input

---
+++ augbatch5/src/day1_hello_world/AddTwoNumbers.java
@@ -0,0 +1,22 @@
+package day1_hello_world;
+
+import java.util.Scanner;
+
+class AddTwoNumbers {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in); // used to taking input from user
+
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
+        int sum = number1 + number2;
+        System.out.println(sum);
+
+
+//        System.out.println(10 + 20);
+//        System.out.println(20 - 10);
+//        System.out.println(20 * 10);
+//        System.out.println(28 / 5);
+//        System.out.println(28 % 5); // remainder modulo operator
+
+    }
+}
---
+++ augbatch5/src/day1_hello_world/Day1Recap.java
@@ -0,0 +1,30 @@
+package day1_hello_world;
+
+class Day1Recap {
+    public static void main(String[] args) {
+        // data type -> suitable container to store data
+        //integer number
+        //data type variable name = value
+        int number = 110;
+        long number2 = 999;
+
+        //decimal number
+        double number3 = 10.5;
+        float number4 = 10.9f; // mandatory to use f at the end
+
+        char gender = 'M'; // single quotes is a character
+
+        // 0/1
+        boolean var1 = true; //1 ON
+        boolean var2 = false; //0 OFF
+
+        byte number5 = 122;
+        short number6 = 199;
+
+        //output?
+        String name = "Newton";
+        System.out.println(name);
+
+
+    }
+}
