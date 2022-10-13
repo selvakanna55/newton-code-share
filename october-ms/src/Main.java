e9180e25cbbe03911e2a42691d8bb7afc4f71697
selvamanitce <selvakanna2409@gmail.com>
2022-10-13 23:11:46 +0530

    add basics

--- october-ms/src/Main.java
+++ october-ms/src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Main {
+    public static void main(String args[]) { // String args[] -> command line args
+        System.out.println("Hello world");  // this line show Hello world in the display
+        System.out.println("10+20"); // why " " for 10+20
+        System.out.println(10 + 20);
+        System.out.println(10 * 9);
+        System.out.println(66 / 11);
+        System.out.println(24 - 14);
+
+        // Store student information in java program
+        //name: Newton -> String (set of chars)
+        // age: 25 -> number
+        // gender: m -> character
+        // percentage: 99.9 -> float
+        //
+        // data types
+
+
     }
 }
