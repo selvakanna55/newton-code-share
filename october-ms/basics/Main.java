a98ff0d825d96657f07286edbbc9c1a1475f895a
selvamanitce <72291160+selvamanitce@users.noreply.github.com>
2022-10-15 23:31:18 +0530

    Create Main.java

---
+++ october-ms/basics/Main.java
@@ -0,0 +1,108 @@
+class Main{ 
+    public static void main(String args[]){ 
+        // System.out.println("Hello world");  // this line show Hello world in the display
+        // // how to store student data
+
+        // int rollNumber = 10; // assignment statemnet
+        // float percentage = 99.5f; // assignment statemnet
+        // percentage = 100f; // assignment statemnet
+
+        // char gender = 'm'; 
+        // String name = "newton";
+
+        // System.out.println(rollNumber);
+        // System.out.println(percentage);
+        // System.out.println(gender);
+        // System.out.println(name);
+        // // name = newton
+        // // rollno = 10 (1010 bits 4bits)   32 100000
+        // // gender = m
+        // //percentage = 99.5
+
+        // // square -> a*a
+        // // side- > 4cm
+        // // a = 4;
+        // // a*a = 16
+        
+
+
+        // int number1 = 10;
+        // int number2 = 20;
+        // System.out.println(number1+number2);
+        // System.out.println(number2-number1);
+        // System.out.println(number2*number1);
+        // System.out.println(number2/number1);
+
+        // int a = 10;
+        // int b = 3;
+        // System.out.println(a/b); //10/3 -> 3
+        // System.out.println(a%b); //10%3 -> 1
+
+        // System.out.println(14/5);
+        // System.out.println(14%5); 
+
+        // double a = 14;
+        // double b = 5;
+        // System.out.println(a/b); // 2 
+
+        // char firstLetter = 'a';
+        // char secondLetter = 'b';
+        // System.out.println(firstLetter);
+        // System.out.println(secondLetter);
+
+
+        // int num = 1000000000; // 10^9
+        // System.out.println(num);
+        // long num2 = 1000000000000000000L; //10^19
+        // System.out.println(num2);
+        
+
+        // // float a = 10.9534345678654678654678f;
+        // // double b = 10.95343456786546786546787654677656;
+        // // System.out.println(a);
+        // // System.out.println(b);
+
+        // // boolean  a1 = false;
+        // // boolean a2 = true;
+        // // System.out.println(a1);
+        // // System.out.println(a2);
+
+        // // // byte m = 7;
+        // // // System.out.println(m);
+        // // int a; // declaration
+        // // a = 10; // assignment
+        // // int b = 40;
+        // // System.out.println(b);
+    
+        // int ans = 10; // associativy
+        // System.out.println(ans);
+        // ans = ans+10; // add 1
+        // System.out.println(ans);
+        // ans++;// add 1 post increment
+        // System.out.println(ans);
+        // ans+=10; //shorthand ans +=1
+        // System.out.println(ans);
+      // == -> comparision
+      // = assignment
+
+        // System.out.println(10!=(5*2));
+      //+, -, /, *, %
+    int a = 10;
+    System.out.println(a);    
+    a = a + 40; 
+    System.out.println(a);
+    a +=40;
+    System.out.println(a);
+    a++; // a = a+1
+    System.out.println(a);
+
+    System.out.println(10>2);
+    System.out.println(10==(2*5));
+    System.out.println(10!=(2*5)); //10 is not equal to 10
+    System.out.println(10<9);
+
+
+
+    
+    }
+}
