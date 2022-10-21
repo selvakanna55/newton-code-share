fd6d41f9310d3907bb9ec77ffb58996aec654022
selvamanitce <selvakanna2409@gmail.com>
2022-10-21 20:49:06 +0530

    added spring security

---
+++ jan-java-spring-2/college/src/main/java/com/college/college/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.college.college;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("abhishek").password("abhishek").roles("admin");
+        auth.inMemoryAuthentication().withUser("jai").password("jai").roles("professor");
+        auth.inMemoryAuthentication().withUser("newton").password("newton").roles("admin");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/departments").hasRole("admin")
+                .antMatchers("/courses").hasRole("professor")
+                .and().formLogin();
+    }
+}
+
