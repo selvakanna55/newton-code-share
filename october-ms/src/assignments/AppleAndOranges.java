c9a595be85e6ea288e91d4e6b65a1eb7b941da31
selvamanitce <selvakanna2409@gmail.com>
2022-10-29 23:37:29 +0530

    assignment

---
+++ october-ms/src/assignments/AppleAndOranges.java
@@ -0,0 +1,8 @@
+package assignments;
+
+public class AppleAndOranges {
+    static int LikesBoth(int N, int A, int B) {
+        int sum = A + B - N;
+        return sum;
+    }
+}
---
+++ october-ms/src/assignments/TriangleRightAngle.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class TriangleRightAngle {
+    static void printTriangle() {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
