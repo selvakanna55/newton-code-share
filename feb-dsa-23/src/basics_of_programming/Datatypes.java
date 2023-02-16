62e485cfbc426176feff0bac8c85392f1948d8b4
selvamanitce <selvakanna2409@gmail.com>
2023-02-17 00:27:55 +0530

    datatype

---
+++ feb-dsa-23/src/basics_of_programming/Datatypes.java
@@ -0,0 +1,47 @@
+package basics_of_programming;
+
+class Datatypes {
+    public static void main(String args[]) {
+        short number1 = 10;
+
+        int intVariable = 56;
+        System.out.println(intVariable);
+
+
+        long number3 = 105678765;
+
+        float percentage = 99.8f;
+        double average = 45.77777771777475;
+
+        boolean isPlaced = true;
+
+        byte num = 10;
+
+        char geneder = 'M';
+        char symbol = '$';
+
+        System.out.println(number1);
+        System.out.println(number1);
+        System.out.println(number3);
+        System.out.println(percentage);
+        System.out.println(average);
+        System.out.println(isPlaced);
+        System.out.println(num);
+        System.out.println(geneder);
+        System.out.println(symbol);
+
+        //char -> 1byte
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ feb-dsa-23/src/basics_of_programming/PrintData.java
@@ -0,0 +1,31 @@
+package basics_of_programming;
+
+
+class PrintData {
+    public static void main(String args[]) {
+        int a = 10;
+        long b = 20;
+        float c = 30.9879f;
+        double d = 99.741852;
+        char e = 'z';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.printf("%.2f\n", c);
+        System.out.printf("%.4f\n", d);
+        System.out.println(e);
+
+
+        System.out.println("Hello world");
+        System.out.println("how are you?");
+        System.out.println("10+20 $#@"); // 30
+        System.out.println(10 + 20);
+        System.out.println(10 * 9);
+        System.out.println(10 / 2);
+        System.out.println(10 / 2);
+        System.out.println(5 * (4 + 10));
+        System.out.println(10 / 3); // 3.33
+
+
+    }
+}
