3aa3c97aaecc33cf98f2422c6d38d10492d22cb1
selvamanitce <selvakanna2409@gmail.com>
2023-02-20 00:48:29 +0530

    feb dsa scanner

---
+++ feb-dsa-23/src/input_output/OutErr.java
@@ -0,0 +1,8 @@
+package input_output;
+
+public class OutErr {
+    public static void main(String[] args) {
+        System.out.println("output tab");
+        System.err.println("error tab");
+    }
+}
