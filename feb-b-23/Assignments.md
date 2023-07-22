9bcb67364fb24a9ec443d9d3e8790c08f22d2c39
selvamanitce <selvakanna2409@gmail.com>
2023-07-22 23:22:26 +0530

    linkedlist - insert, delete

---
+++ feb-b-23/Assignments.md
@@ -0,0 +1,2 @@
+Insert node at the given position - https://my.newtonschool.co/playground/code/0e0j1xx12mc0
+Print the Linked List - https://my.newtonschool.co/playground/code/zumrcob6385q
--- feb-b-23/src/linkedlist/LinkedListExample.java
+++ feb-b-23/src/linkedlist/LinkedListExample.java
@@ -1,6 +1,5 @@
 package linkedlist;
 
-
 class Node {
     int value;
     Node next;
@@ -10,31 +9,101 @@ class Node {
     }
 }
 
-public class LinkedListExample {
-    static void print(Node head) {
+class LinkedList {
+    Node head;
+
+    void printList() {
         Node curr = head;
         while (curr != null) {
-            System.out.println(curr.value);
+            System.out.print(curr.value + " ");
             curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    void insertAtPos(int pos, int num) {
+        Node newNode = new Node(num);
+        if (pos == 1) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node curr = head;
+            for (int i = 2; i < pos; i++) {
+                curr = curr.next;
+            }
+            newNode.next = curr.next;
+            curr.next = newNode;
         }
     }
 
-    static void printNthElement(Node head, int idx) {
-        // array -> arr[idx] -> O(1)
-        // 0 based index
-        Node curr = head; // O(n)
-        for (int i = 1; i <= idx; i++) {
-            curr = curr.next;
+    void insertAtEnd(int num) {
+        Node newNode = new Node(num);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node curr = head;
+            // move curr to the last node
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
         }
-        System.out.println(curr.value);
     }
+
+
+    void deletePos(int pos) {
+        if (pos == 1) {
+            head = head.next;
+        } else {
+            Node curr = head;
+            for (int i = 2; i < pos; i++) {
+                curr = curr.next;
+            }
+            curr.next = curr.next.next;
+        }
+    }
+
+}
+
+public class LinkedListExample {
 
     public static void main(String args[]) {
-        Node head = new Node(10);
-        head.next = new Node(20);
-        head.next.next = new Node(30);
-        head.next.next.next = new Node(40);
-//        print(head);
-        printNthElement(head, 0);
+        LinkedList l1 = new LinkedList();
+        l1.insertAtPos(1, 10);
+        l1.insertAtPos(2, 20);
+        l1.insertAtPos(3, 30);
+        l1.insertAtPos(4, 40);
+        l1.insertAtPos(5, 50);
+        l1.printList();
+        l1.deletePos(4); //delete middle nodes
+        l1.printList();
+        l1.deletePos(4); // delete last node
+        l1.printList();
+        l1.deletePos(1); // delete head
+        l1.printList();
+
+//
+//        Node head = new Node(10);
+//        head.next = new Node(20);
+//        head.next.next = new Node(30);
+//        head.next.next.next = new Node(40);
+
+
+//        l1.insertAtPos(4, 33);
+//        l1.insertAtPos(1, 9); // insert at head
+//        l1.printList();
+
+
+        // Scanner sc = new Scanner(System.in);
+        // // int n  =  sc.nextInt();
+        // // for(int i=0;i<n;i++){
+        // //     int num = sc.nextInt();
+        // //     l1.insertAtEnd(num);
+        // // }
+        // // l1.printList();
     }
 }
+
+
+
+
